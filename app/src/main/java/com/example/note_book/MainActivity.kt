b6b95e0d8d620384a91131b51d7b7600c250b5f2
RMashkalev <rmashkalev@mail.ru>
2024-10-17 15:28:11 +0700

    Новый проект

---
+++ app/src/main/java/com/example/note_book/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.note_book
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.note_book.ui.theme.NotebookTheme
+
+class MainActivity : ComponentActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			NotebookTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android",
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	NotebookTheme {
+		Greeting("Android")
+	}
+}
