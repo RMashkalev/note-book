ea91053d81d5f0d11abceecff66ae672b832ec27
RMashkalev <rmashkalev@mail.ru>
2025-04-02 18:04:02 +0700

    Добавил анимацию смена цвета

--- feature/home/src/main/java/com/example/home/ui/component/Note.kt
+++ feature/home/src/main/java/com/example/home/ui/component/Note.kt
@@ -3,7 +3,11 @@ package com.example.home.ui.component
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,27 +23,37 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorLong
 import com.example.home.R
 import com.example.notedatabase.domain.entity.Note
 import com.example.ui.theme.NotebookTheme
 import com.example.ui.theme.Typography
+import kotlinx.coroutines.delay
+
+private const val COLOR_CHANGE_TIME = 500
 
 @Composable
 fun Note(
@@ -53,8 +67,9 @@ fun Note(
 	onColorChange: (Note) -> Unit,
 ) {
 	var expanded by remember { mutableStateOf(false) }
+	var gradientAnimate by remember { mutableStateOf(false) }
 	val columnHeight by animateDpAsState(
-		targetValue = if (expanded) 160.dp else 96.dp,
+		targetValue = if (expanded) 200.dp else 96.dp,
 		animationSpec = tween(
 			durationMillis = 1000,
 			easing = FastOutSlowInEasing
@@ -65,14 +80,14 @@ fun Note(
 	val newFirstColor by animateColorAsState(
 		targetValue = Colors.entries[firstColorIndex].color,
 		animationSpec = tween(
-			durationMillis = 500,
+			durationMillis = COLOR_CHANGE_TIME,
 			easing = LinearEasing
 		)
 	)
 	val newSecondColor by animateColorAsState(
 		targetValue = Colors.entries[secondColorIndex].color,
 		animationSpec = tween(
-			durationMillis = 500,
+			durationMillis = COLOR_CHANGE_TIME,
 			easing = LinearEasing
 		)
 	)
@@ -86,6 +101,17 @@ fun Note(
 					)
 				)
 		)
+	}
+
+	LaunchedEffect(gradientAnimate) {
+		while (gradientAnimate) {
+			delay(COLOR_CHANGE_TIME.toLong())
+			val tmp = firstColorIndex
+			firstColorIndex = secondColorIndex
+			secondColorIndex = tmp
+		}
+		firstColorIndex = firstColor.toInt()
+		secondColorIndex = secondColor.toInt()
 	}
 
 	Column(
@@ -136,60 +162,79 @@ fun Note(
 				.padding(top = 12.dp)
 		)
 
-		HorizontalDivider(
-			color = Color.Gray
-		)
+		if (columnHeight > 96.dp) {
+			HorizontalDivider(
+				color = Color.Gray
+			)
 
-		Row(
-			modifier = Modifier
-				.fillMaxWidth(),
-		) {
-			Colors.entries.forEach { color ->
-				Box(
-					modifier = Modifier
-						.padding(start = 3.dp)
-						.size(32.dp)
-						.background(color = color.color)
-						.clickable {
-							firstColorIndex = color.ordinal
-							onColorChange(
-								Note(
-									id = id,
-									title = title,
-									description = description,
-									firstColor = firstColorIndex.toLong(),
-									secondColor = secondColorIndex.toLong(),
+			Row(
+				modifier = Modifier
+					.fillMaxWidth(),
+			) {
+				Colors.entries.forEach { color ->
+					Box(
+						modifier = Modifier
+							.padding(start = 3.dp)
+							.size(32.dp)
+							.background(color = color.color)
+							.clickable {
+								firstColorIndex = color.ordinal
+								onColorChange(
+									Note(
+										id = id,
+										title = title,
+										description = description,
+										firstColor = firstColorIndex.toLong(),
+										secondColor = secondColorIndex.toLong(),
+									)
 								)
-							)
-						}
-				)
+							}
+					)
+				}
 			}
-		}
 
-		HorizontalDivider()
+			Spacer(modifier = Modifier.padding(1.dp))
 
-		Row(
-			modifier = Modifier
-				.fillMaxWidth(),
-		) {
-			Colors.entries.forEach { color ->
-				Box(
-					modifier = Modifier
-						.padding(start = 3.dp)
-						.size(32.dp)
-						.background(color = color.color)
-						.clickable {
-							secondColorIndex = color.ordinal
-							onColorChange(
-								Note(
-									id = id,
-									title = title,
-									description = description,
-									firstColor = firstColorIndex.toLong(),
-									secondColor = secondColorIndex.toLong(),
+			Row(
+				modifier = Modifier
+					.fillMaxWidth(),
+			) {
+				Colors.entries.forEach { color ->
+					Box(
+						modifier = Modifier
+							.padding(start = 3.dp)
+							.size(32.dp)
+							.background(color = color.color)
+							.clickable {
+								secondColorIndex = color.ordinal
+								onColorChange(
+									Note(
+										id = id,
+										title = title,
+										description = description,
+										firstColor = firstColorIndex.toLong(),
+										secondColor = secondColorIndex.toLong(),
+									)
 								)
-							)
-						}
+							}
+					)
+				}
+			}
+
+			Row(
+				modifier = Modifier
+					.fillMaxWidth(),
+				verticalAlignment = Alignment.CenterVertically,
+			) {
+				Checkbox(
+					checked = gradientAnimate,
+					onCheckedChange = {
+						gradientAnimate = !gradientAnimate
+					}
+				)
+
+				Text(
+					text = stringResource(id = R.string.gradient_animate_checkbox)
 				)
 			}
 		}
@@ -210,9 +255,10 @@ private enum class Colors(val color: Color) {
 	SAND(Color(0xFFFFF3E0))
 }
 
-private fun colorsToPairs(firstColor: Color, secondColor: Color): Array<Pair<Float, Color>> {
+private fun colorsToPairs(firstColor: Color, secondColor: Color, middleColor: Color = lerp(firstColor, secondColor, 0.5f)): Array<Pair<Float, Color>> {
 	return arrayOf(
 		0f to firstColor,
+		0.5f to middleColor,
 		1f to secondColor,
 	)
 }
